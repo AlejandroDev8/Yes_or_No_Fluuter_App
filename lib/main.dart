ba2952d63ef1589242691352fb32b7993639aea8
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-05-02 23:37:20 -0600

    refactor: Remove commented out code and update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_or_no/config/theme/app_theme.dart';
+import 'package:yes_or_no/presentation/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,15 +13,16 @@ class MyApp extends StatelessWidget {
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme().theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: FilledButton.tonal(
-                onPressed: () {}, child: const Text('Click me!'))),
-      ),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('Material App Bar'),
+      //     centerTitle: true,
+      //   ),
+      //   body: Center(
+      //       child: FilledButton.tonal(
+      //           onPressed: () {}, child: const Text('Click me!'))),
+      // ),
+      home: const ChatScreen(),
     );
   }
 }
